08372bad5bee398c1a55a4df87c68ce6d1dbf525
rainersalens <rainersalens@inbox.lv>
2022-11-28 23:38:49 +0200

    Pushing more recent changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('template.html')
+    return render_template('homepage.html')
 
 @app.route("/stats")
 def stats():
--- static/script.js
+++ static/script.js
@@ -6,16 +6,20 @@ function getInput() {
     }
 }
 
-void async function fetchData(userInputName) {
+async function fetchData(userInputName) {
   console.log(userInputName)
   const url = "https://fortnite-api.com/v2/stats/br/v2?name=" + userInputName + "&accountType=epic&timeWindow=lifetime&image=all";
-//   fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }})
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
+  // fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }})
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data));
 // ----> Had to comment this out to test another method that will work with what I want to do <----
 const response = await fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }});
 const data = await response.json();
-console.log(data.account.name);
+console.log(data);
+
+const { name } = data;
+
+document.getElementById("responseStatus").textContent = name;
 
 }
 
