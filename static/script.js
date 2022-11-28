254f8025845866595c549d63741b6916d85448ea
rainersalens <rainersalens@inbox.lv>
2022-11-29 00:19:23 +0200

    made it possible to use the API responses

--- static/script.js
+++ static/script.js
@@ -7,19 +7,24 @@ function getInput() {
 }
 
 async function fetchData(userInputName) {
-  console.log(userInputName)
+  console.log("Searching for user: " + userInputName)
   const url = "https://fortnite-api.com/v2/stats/br/v2?name=" + userInputName + "&accountType=epic&timeWindow=lifetime&image=all";
   // fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }})
   //   .then((response) => response.json())
   //   .then((data) => console.log(data));
 // ----> Had to comment this out to test another method that will work with what I want to do <----
 const response = await fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }});
-const data = await response.json();
+
+if (response.status != 200) {
+  document.getElementById("responseStatus").textContent = "Request error " + response.status;
+  return;
+}
+
+const data = (await response.json()).data;
+
 console.log(data);
 
-const { name } = data;
-
-document.getElementById("responseStatus").textContent = name;
-
+document.getElementById("responseStatus").textContent = "Account name: " + data.account.name + "Test";
+document.getElementById("resultDiv").style.opacity = 100;
 }
 
