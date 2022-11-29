c5066fe44325b906e7c02d5c1a81c08714f3d7ee
rainersalens <rainersalens@inbox.lv>
2022-11-29 16:06:11 +0200

    Added a timeframe option

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
+// First 2 lines is for the button functionality
+var timeFrame = 1;
+
 function getInput() {
     var userInputName = document.getElementById("usern").value;
     if (userInputName == "") { window.alert("No input found") }
@@ -7,8 +10,13 @@ function getInput() {
 }
 
 async function fetchData(userInputName) {
+  if (timeFrame == 0) {
+    var usedTimeFrame = "season";
+  } else {
+    var usedTimeFrame = "lifetime";
+  }
   console.log("Searching for user: " + userInputName)
-  const url = "https://fortnite-api.com/v2/stats/br/v2?name=" + userInputName + "&accountType=epic&timeWindow=lifetime&image=all";
+  const url = "https://fortnite-api.com/v2/stats/br/v2?name=" + userInputName + "&accountType=epic&timeWindow=" + usedTimeFrame + "&image=all";
   // fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }})
   //   .then((response) => response.json())
   //   .then((data) => console.log(data));
@@ -29,6 +37,7 @@ document.getElementById("responseStatus").innerHTML =
   "Account name: " + data.account.name + "<br>" 
   + "Account ID: " + data.account.id + "<br>"
   + "<br>"
+  + "Showing stats for " + usedTimeFrame + "<br>"
   + "Solo k/d: " + data.stats.all.solo.kd + "<br>"
   + "Duos k/d: " + data.stats.all.duo.kd + "<br>"
   + "Squads k/d: " + data.stats.all.squad.kd + "<br>";
