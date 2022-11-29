2569f0c88f821497b966f2e15065c02e0dc3ca0a
rainersalens <rainersalens@inbox.lv>
2022-11-29 16:36:43 +0200

    ran into issue with null values

--- static/script.js
+++ static/script.js
@@ -17,11 +17,7 @@ async function fetchData(userInputName) {
   }
   console.log("Searching for user: " + userInputName)
   const url = "https://fortnite-api.com/v2/stats/br/v2?name=" + userInputName + "&accountType=epic&timeWindow=" + usedTimeFrame + "&image=all";
-  // fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }})
-  //   .then((response) => response.json())
-  //   .then((data) => console.log(data));
-// ----> Had to comment this out to test another method that will work with what I want to do <----
-const response = await fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }});
+  const response = await fetch(url, { headers: { "Authorization": "d598c2c0-0ef7-40e9-ba23-b49a34262d50" }});
 
 if (response.status != 200) {
   document.getElementById("responseStatus").textContent = "Request error " + response.status;
